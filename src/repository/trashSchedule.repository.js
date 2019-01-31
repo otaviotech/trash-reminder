57a671fbaedaf91c31ff7ed7728f06e2e56166f3
Otávio Araújo <otaviotech@gmail.com>
2019-01-31 18:08:59 -0200

    [firebase] Correções no service do firebase.

--- src/repository/trashSchedule.repository.js
+++ src/repository/trashSchedule.repository.js
@@ -1,4 +1,4 @@
-const db = require('./db');
+const db = require('../service/firebase.service');
 
 const createTrashScheduleRepository = function(getDBConnection = db) {
   return {
--- src/service/firebase.service.js
+++ src/service/firebase.service.js
@@ -2,17 +2,9 @@ const firebase = require('firebase');
 const firebaseConfig = require('../../config').firebase;
 
 function createFirebaseDatabaseConnection() {
-  // Initialize Firebase
-  const config = {
-    apiKey: 'AIzaSyCwnCYgi689lR3WtN22WZIVGfsQVrc3lcQ',
-    authDomain: 'trash-reminder-3ac7f.firebaseapp.com',
-    databaseURL: 'https://trash-reminder-3ac7f.firebaseio.com',
-    projectId: 'trash-reminder-3ac7f',
-    storageBucket: 'trash-reminder-3ac7f.appspot.com',
-    messagingSenderId: '753911454398'
-  };
-
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const firebaseApp = !firebase.apps.length
+    ? firebase.initializeApp(firebaseConfig)
+    : firebase.app();
 
   return firebaseApp.auth().signInAnonymously()
     .then(() => {
@@ -23,17 +15,5 @@ function createFirebaseDatabaseConnection() {
       return Promise.reject('Erro ao conectar com Firebase.');
     });
 }
-
-createFirebaseDatabaseConnection().then((con) => {
-  con.ref('lastRemoval').set({ collaboratorID: 3, date: '2019-01-25' })
-    .then(() => {
-
-      con.ref('lastRemoval').once('value')
-      .then(v => {
-        console.log(v.val());
-        process.exit(0);
-      });
-    });
-});
 
 module.exports = createFirebaseDatabaseConnection;
